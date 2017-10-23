5df591582f6cbc0f71c4e77f9c771fb7f2b897ad
Dregorio <luks9_9@o2.pl>
2017-10-23 22:15:50 +0200

    changed AppSthSth to Activity

--- app/src/main/java/com/example/damis/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/damis/myapplication/Main2Activity.java
@@ -1,19 +1,17 @@
 package com.example.damis.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ListViewCompat;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
